dbdd1ae22f66eaecfe3a67163a6345c55faa6f98
mohamed osama <mohamedmedo9932@gmail.com>
2024-12-02 04:42:31 +0200

    build setting item

--- lib/constants/strings/text.dart
+++ lib/constants/strings/text.dart
@@ -2,3 +2,5 @@ const String whatDoYouWantToDo = 'What do you want to do?';
 const String tapToAdd = 'Tap + to add your tasks';
 const String taskCardItemTitle = 'Do Math Homework';
 const String taskCardItemTime = 'Today At 08:15';
+const String changeAppTheme = 'Change app theme';
+const String changeapplanguage = 'Change app language';
--- lib/screens/profile_screen/profile_screen.dart
+++ lib/screens/profile_screen/profile_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:taski/constants/strings/text.dart';
+import 'package:taski/widgets/app_bar_title.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -7,11 +10,46 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Profile'),
-        automaticallyImplyLeading: false,
-        backgroundColor: const Color(0xff8687E7),
+        title: const AppBarTitle(title: 'Profile'),
       ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            BuildSettingItem(
+                title: changeAppTheme,
+                leadingIcon: FontAwesomeIcons.penToSquare),
+            SizedBox(height: 16),
+            BuildSettingItem(
+                title: changeapplanguage,
+                leadingIcon: FontAwesomeIcons.language),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BuildSettingItem extends StatelessWidget {
+  const BuildSettingItem({
+    super.key,
+    this.onTap,
+    required this.title,
+    required this.leadingIcon,
+    this.trailingIcon = FontAwesomeIcons.chevronRight,
+  });
+  final void Function()? onTap;
+  final String title;
+  final IconData leadingIcon;
+  final IconData trailingIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(leadingIcon),
+      title: Text(title, style: Theme.of(context).textTheme.headlineMedium),
+      trailing: Icon(trailingIcon),
+      onTap: onTap,
     );
   }
 }
