cf770466a9c89e05a9fcee895d752a174c8fc8ad
mohamed osama <mohamedmedo9932@gmail.com>
2024-12-11 11:56:10 +0200

    refector custom floating action button

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:taski/bloc/get_tasks_cubit/cubit/get_tasks_cubit.dart';
-import 'package:taski/constants/colors/my_colors.dart';
 import 'package:taski/constants/strings/routes.dart';
 import 'package:taski/models/tasks/task_model.dart';
 import 'package:taski/widgets/app_bar_title.dart';
 import 'package:taski/widgets/home_screen/check_if_there_is_taskin_home_screen_or_not.dart';
+import 'package:taski/widgets/home_screen/custom_floating_action_button.dart';
 import 'package:taski/widgets/home_screen/custom_list_to_view_tasks.dart';
 import 'package:taski/widgets/home_screen/search_bar_in_home_screen.dart';
 
@@ -25,13 +23,8 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     List<TaskModel> tasks = BlocProvider.of<GetTasksCubit>(context).tasks ?? [];
     return Scaffold(
-      floatingActionButton: customFloatingActionButton(
-        context,
-        'Add Task',
-        () {
-          Navigator.of(context, rootNavigator: true).pushNamed(addTaskScreen);
-        },
-      ),
+      // ! Floating Action Button try in the future delete navigate to another screen and cereate bottom sheet
+      floatingActionButton: customFloatingActionButton(context),
       appBar: AppBar(title: const AppBarTitle(title: 'Home')),
       body: Padding(
         padding: const EdgeInsets.all(16),
@@ -53,15 +46,6 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       ),
-    );
-  }
-
-  FloatingActionButton customFloatingActionButton(
-      BuildContext context, String title, void Function()? onPressed) {
-    return FloatingActionButton(
-      tooltip: title,
-      onPressed: onPressed,
-      child: const Icon(Icons.add, color: MyColors.lightColor),
     );
   }
 }
